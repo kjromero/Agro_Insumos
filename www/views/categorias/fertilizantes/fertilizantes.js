2986157e648247211db11a2540d5f79c5c7adab5
Kenny Romero <sastoque.kenny@gmail.com>
2015-10-22 10:28:38 -0500

    Add fonts

---
+++ www/views/categorias/fertilizantes/fertilizantes.js
@@ -0,0 +1,9 @@
+angular.module('starter')
+.controller('FertilizantesController', function($scope) {
+  $scope.insumo = {
+    name: "Agroland Vitta +",
+    precio_m: "$85.000",
+    precio_n: "$83.000",
+    precio_d: "$75.000",
+  };
+});
